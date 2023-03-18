c83c5e67044ba44328bdda9893e0dfd2ec7b5ad6
Kieran burnett <kieran.burnett1@hotmail.co.uk>
2023-03-18 21:07:03 +0000

    halfway writing binary search

--- Sort_Search/Searches.cs
+++ Sort_Search/Searches.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace Sort_Search
 {
 	class Searches
 	{
+		// o(n)
 		public static int[] Linear_Search(int[] arr, int value)
 		{
 			Console.WriteLine("Searching for "+value);
@@ -40,6 +42,8 @@ namespace Sort_Search
 			}
 			return list.ToArray();
 		}
+		// as binary only finds one value, could then look left and right however than results in o(n) for large sample sizes
+		// instead do recursive binary seach each side to remain with o(log(n))
 		public static int[] Binary_Search(int[] arr, int value)
 		{
 			Console.WriteLine("Searching for " + value);
@@ -52,6 +56,17 @@ namespace Sort_Search
 			{
 				if (sorted[mid] == value)
 				{
+					// begin recursive binary search above and below
+					int upperBound = mid - 1;
+					int i = upperBound;
+					do
+					{
+						upperBound = i;
+						// doesnt work as Binary_search returns an array but i is an int, mean bottom index and top index
+						// will have to be stored outside the method
+						// to go for values above and below it will have to be a for loop until its broken if a value is found
+						i = Binary_Search(arr, value);
+					} while (i != -1)
 					list.Add(mid);
 					break;
 				}
@@ -65,25 +80,8 @@ namespace Sort_Search
 				}
 				mid = (min + max) / 2;
 			}
-			// if list is empty,try using the number above and below the value
-			if (list.Count == 0)
-			{
-				int above = value + 1;
-				int below = value - 1;
-				while (list.Count == 0)
-				{
-					Console.WriteLine("Now looking for " + above + " and " + below + ".");
-					for (int i = 0; i < sorted.Length; i++)
-					{
-						if (sorted[i] == above || sorted[i] == below)
-						{
-							list.Add(i);
-						}
-					}
-					above++;
-					below--;
-				}
-			}
+			// if list is empty,try using the number above and below the value,
+			
 			return list.ToArray();
 		}
 	}
